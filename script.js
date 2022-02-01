64d1c44fb9e42b9fb81b0a4dfdfc21845c5bece7
Dev Karan <81677957+Kev-Daran@users.noreply.github.com>
2022-02-01 21:13:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -25,6 +25,8 @@ overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
   if (e.key == 'Escape') {
-    closeModal();
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
   }
 });
